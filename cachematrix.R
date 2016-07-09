fd78a6ed85ac7b267236968f3a782c14ba5b61b9
alexandria <alexandria8611@gmail.com>
2016-07-08 23:53:26 -0500

    Adding modified file

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-makeCasheMatrix <- function(x = matrix) {
-  inv<-NULL
-  set<-function(y) {
+##Create makeCacheMatrix
+makeCacheMatrix <- function(x = matrix) {
+  
+##setting the mean to NULL as a placeholder for a future value
+   inv<-NULL
+   
+##defines a function to set the vector, x,  to a new vector, y, and resets the inv to NULL
+  
+   set<-function(y) {
     x<<-y
     inv<<-NULL
-  }git commitction() x
+   }
+   ## Returning the vector x
+get<-function() x
+## Setting the inverse inv
 set_inverse<-function(inverse) inv<<-inverse
+##returning inv
 get_inverse<-function() inv
 list(set=set, get=get, 
      set_inverse = set_inverse, get_inverse = get_inverse)
